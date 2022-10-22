1a54cabe282d80346782a50957411ab4d40ed040
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2022-10-22 16:26:09 +0100

    Completed Intro page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Intro from '../views/Intro.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'Intro',
+      component: Intro
+    },
+    {
+      path: '/home',
       name: 'home',
       component: Home
-    },
+    }
   ]
 })
 
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,5 +1,35 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
 
 <template>
-    <h1>Hello World</h1>
+    <main class="bg-white relative w-full h-full">
+        <span class="inline-block w-[350px] h-[350px] bg-pry absolute opacity-10 blur-lg rounded-full -top-24 -left-10"></span>
+        <span class="inline-block w-[300px] h-[300px] bg-pry absolute opacity-10 blur-lg rounded-full -right-5 top-24"></span>
+        
+        
+        <img class="absolute right-8 -top-10 opacity-50" src="@/assets/images/icons/Leaf.svg" alt="Leaf">
+        
+        <img class="absolute -right-4 bottom-0 blur-sm opacity-80" src="@/assets/images/icons/Leaf.svg" alt="Leaf">
+        
+        <img class="absolute rotate-90 left-10 w-10 h-10 -bottom-24 z-10 opacity-50" src="@/assets/images/icons/Leaf.svg" alt="Leaf">
+        
+        
+        
+        <article class="text-center mt-[15%] w-[80%] mx-auto">
+            <img class="mb-[32px] mx-auto" src="@/assets/images/icons/logo - Circle.svg" alt="Logo">
+
+            <h1 class="text-3xl leading-10 font-bold text-dark mb-4">Get your groceries delivered to your home</h1>
+
+            <p class="text-lg text-grey w-[85%] mx-auto mb-8">The best delivery app in town for delivering your daily fresh groceries</p>
+
+            <button class="bg-pry text-base text-white font-bold w-1/2 mx-auto px-5 py-3 rounded-full cursor-pointer" @click="$router.push('/home')"> 
+                Shop now
+            </button>
+        </article>
+
+        <div class="bg-sec w-full h-[45%] fixed bottom-0">
+            <img class="w-full h-full object-cover"  src="@/assets/images/onbording img.jpg" alt="Foods">
+        </div>
+    </main>
 </template>
